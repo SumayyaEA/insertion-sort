720e0fa17631995fd215392add930c8eb08c5320
SUMAYYA E A <04614@user>
2017-06-23 20:26:31 +0530

    read function added to read array

--- insertion.c
+++ insertion.c
@@ -1,11 +1,8 @@
 #include<stdio.h>
 
-
-
-int main()
+int sort(int *a)
 {
 int i,j,temp;
-int a[7]={4,23,43,38,15,10,30};
 for(i=0;i<7;i++)
 {
 	j=i;
@@ -17,9 +14,26 @@ for(i=0;i<7;i++)
 		j=j-1;
 	}
 }
-for(i=0;i<7;i++)
-{
-printf("%d\t",a[i]);
+return 0;
 }
+
+int read(int *a)
+{
+int size,i;
+scanf("%d",&size);
+for(i=0;i<size;i++)
+	scanf("%d",&a[i]);
+return size;
+}
+
+int main()
+{
+int i;
+int a[7]={4,23,43,38,15,10,30};
+
+sort(a);
+for(i=0;i<7;i++)
+printf("%d\t",a[i]);
 printf("\n");
+return 0;
 }
